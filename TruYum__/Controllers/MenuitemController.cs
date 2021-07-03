907b2b7175743ce4e5af648388b87362c7ec057b
thilak15 <thilakreddy125@gmail.com>
2021-07-03 13:59:30 +0530

    Add project files.

---
+++ TruYum__/Controllers/MenuitemController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using TruYum__.Models;
+
+namespace TruYum__.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MenuitemController : ControllerBase
+    {
+        // GET: api/<MenuItemController>
+        [HttpGet]
+        public IEnumerable<Menuitem> Get()
+        {
+            return new Menuitem[] {
+                new Menuitem() {Id=1,Name="Biryani",freeDelivery=true,Price=1000.00,dateOfLaunch=Convert.ToDateTime("2000-02-01"),Active=true },
+                new Menuitem() {Id=2,Name="Fish",freeDelivery=false,Price=1250.00,dateOfLaunch=Convert.ToDateTime("2001-02-01"),Active=false },
+                new Menuitem() {Id=3,Name="Dessert",freeDelivery=true,Price=1500.00,dateOfLaunch=Convert.ToDateTime("1999-09-15"),Active=true },
+
+            };
+        }
+
+        // GET api/<MenuItemController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            foreach (var i in Get())
+            {
+                if (i.Id == id)
+                {
+                    return i.Name;
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ TruYum__/Models/Menuitem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TruYum__.Models
+{
+    public class Menuitem
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; }
+        [Display(Name = "Free Delivery")]
+        public bool freeDelivery { get; set; }
+        [Required]
+        public double Price { get; set; }
+        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
+        public DateTime dateOfLaunch { get; set; }
+        public bool Active { get; set; }
+    }
+}
